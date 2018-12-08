a58558fbe4e6e0c17529d5c675b96510ba0c8746
Michael Greene <michael.greene@microsoft.com>
2018-12-08 15:21:28 -0600

    test calling function

--- CompositeResource/run.ps1
+++ CompositeResource/run.ps1
@@ -1,5 +1,5 @@
 $request = Get-Content $req -Raw | ConvertFrom-Json
- 
+
 if ($req_query_config) {
     $config = $req_query_config
 }
@@ -7,8 +7,12 @@ else {
     $config = $requestBody.config
 }
 
+import-module .\compositeresource\compositeresource.psd1
+ConvertTo-CompositeResource -configurationname $config -author 'function'
+$out = ls .\
+
 $response = @{
-    Config = $config
+    Config = $out
 } | ConvertTo-Json
  
 Out-File -InputObject $response -FilePath $res -Encoding Ascii
